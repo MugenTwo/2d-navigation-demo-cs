0ada009415238ee16e5eb22fcde73ee76ccbe2fb
MugenTwo <johncarlo.purihin@gmail.com>
2020-07-16 19:48:19 +0200

    Updating deprecated methods

--- Navigation.cs
+++ Navigation.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 
 public class Navigation : Navigation2D
 {
-
     [Export]
-    private float characterSpeed = 350;
-    private List<Vector2> path;
+    private float _characterSpeed = 350;
+    private List<Vector2> _path;
 
     public override void _Ready()
     {
-        path = new List<Vector2>();
+        _path = new List<Vector2>();
     }
 
     public override void _Input(InputEvent inputEvent)
@@ -20,43 +19,42 @@ public class Navigation : Navigation2D
             return;
         }
         Sprite character = GetNode("Character") as Sprite;
-        UpdateNavigationPath(character.GetPosition(), GetLocalMousePosition());
+        UpdateNavigationPath(character.Position, GetLocalMousePosition());
     }
 
     public override void _Process(float delta)
     {
-        float walkDistance = delta * characterSpeed;
+        float walkDistance = delta * _characterSpeed;
         MoveAlongPath(walkDistance);
     }
 
     private void UpdateNavigationPath(Vector2 startPosition, Vector2 endPosition)
     {
-        path.AddRange(GetSimplePath(startPosition, endPosition, true));
+        _path.AddRange(GetSimplePath(startPosition, endPosition, true));
         SetProcess(true);
     }
 
     private void MoveAlongPath(float walkDistance)
     {
         Sprite character = GetNode("Character") as Sprite;
-        Vector2 lastPosition = character.GetPosition();
+        Vector2 lastPosition = character.Position;
 
-        while (path.Count != 0)
+        while (_path.Count != 0)
         {
-            float distanceBetweenPoints = lastPosition.DistanceTo(path[0]);
+            float distanceBetweenPoints = lastPosition.DistanceTo(_path[0]);
 
             if (walkDistance < distanceBetweenPoints)
             {
-                character.SetPosition(lastPosition.LinearInterpolate(path[0], walkDistance / distanceBetweenPoints));
+                character.Position = lastPosition.LinearInterpolate(_path[0], walkDistance / distanceBetweenPoints);
                 return;
             }
 
             walkDistance -= distanceBetweenPoints;
-            lastPosition = path[0];
-            path.Remove(lastPosition);
+            lastPosition = _path[0];
+            _path.Remove(lastPosition);
         }
 
-        character.SetPosition(lastPosition);
+        character.Position = lastPosition;
         SetProcess(false);
     }
-
 }
